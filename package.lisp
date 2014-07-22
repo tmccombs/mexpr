19aa3e4b5f8b801f0ba1f460d38a4de4eace7f6e
Thayne McCombs <astrothayne@gmail.com>
2014-07-21 18:33:36 -0600

    Add license

--- package.lisp
+++ package.lisp
@@ -1,3 +1,9 @@
+;; This software is Copyright (c) Thayne McCombs, 2014.
+;; Thayne McCombs grants you the rights to distribute
+;; and use this software as governed by the terms
+;; of the Lisp Lesser GNU Public License
+;; (http://opensource.franz.com/preamble.html),
+;; known as the LLGPL.
 (in-package :cl-user)
 
 (defpackage :bytecurry.mexpr
